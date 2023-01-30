42c55c334f8ec50fd8bf3e6679f8abea23115079
Swarna Mallik <u351215@wns.com>
2023-01-30 14:20:46 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Text summarizer")
+input_text = st.text_area(label='Enter full text:', value="", height=250)
+st.button("submit")
+output_text = st.text_area(label='Summarized text:', value="", height=250)
+
